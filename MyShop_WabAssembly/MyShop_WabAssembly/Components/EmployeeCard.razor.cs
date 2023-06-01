fd0c138b244c3efbef493da429c0559844fd0aca
Simon B.Stirling <simonsbs@gmail.com>
2023-06-01 19:02:09 +0300

    pre events

--- MyShop_WabAssembly/MyShop_WabAssembly/Components/EmployeeCard.razor.cs
+++ MyShop_WabAssembly/MyShop_WabAssembly/Components/EmployeeCard.razor.cs
@@ -1,9 +1,31 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using MyShop.Shared;
 
 namespace MyShop.UI.WebAssembly.Components {
     public partial class EmployeeCard {
         [Parameter]
         public Employee InputEmployee { get; set; }
+
+        protected override void OnInitialized() {
+            base.OnInitialized();
+        }
+
+        protected override void OnParametersSet() {
+            base.OnParametersSet();
+        }
+
+        protected override void OnAfterRender(bool firstRender) {
+            base.OnAfterRender(firstRender);
+        }
+
+        public void CalledPopup(MouseEventArgs e, int id) {
+
+        }
+
+        /*public void CalledFromKeyboard(KeyboardEventArgs e) {
+
+        }*/
     }
+
 }
---
+++ MyShop_WabAssembly/MyShop_WabAssembly/Components/EmployeePopup.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MyShop.Shared;
+
+namespace MyShop.UI.WebAssembly.Components {
+    public partial class EmployeePopup {
+        private Employee? _employee;
+
+        [Parameter]
+        public Employee? Employee { get; set; }
+
+        protected override void OnParametersSet() {
+            base.OnParametersSet();
+
+            _employee = Employee;
+        }
+    }
+}
