80cfe0eb51338fcedeee19dee44b4e521252c4c3
Simon B.Stirling <simonsbs@gmail.com>
2023-06-05 18:03:21 +0300

    Finished api setup

---
+++ MyShop_WabAssembly/MyShop_API/Migrations/20230605150201_InitState.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyShop.API.Migrations
+{
+    public partial class InitState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "ID", "FirstName", "Image", "LastName" },
+                values: new object[] { 1, "Simon", "https://picsum.photos/id/237/300/300", "Stirling" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "ID", "FirstName", "Image", "LastName" },
+                values: new object[] { 2, "Bob", "https://picsum.photos/id/238/300/300", "Smith" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "ID", "FirstName", "Image", "LastName" },
+                values: new object[] { 3, "Jane", "https://picsum.photos/id/239/300/300", "Doe" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
