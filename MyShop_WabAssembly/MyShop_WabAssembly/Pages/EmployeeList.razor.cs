95546493e5eec8c83893aa25b209e04ef2c2fa11
Simon B.Stirling <simonsbs@gmail.com>
2023-06-05 18:57:02 +0300

    Added HttpClient (V1)

--- MyShop_WabAssembly/MyShop_WabAssembly/Pages/EmployeeList.razor.cs
+++ MyShop_WabAssembly/MyShop_WabAssembly/Pages/EmployeeList.razor.cs
@@ -1,15 +1,25 @@
-﻿using MyShop.Shared;
+﻿using Microsoft.AspNetCore.Components;
+using MyShop.Shared;
+using System.Net.Http.Json;
 
 namespace MyShop.UI.WebAssembly.Pages {
     public partial class EmployeeList {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
         private Employee _selectedEmployee;
 
         public List<Employee> Employees { get; set; }
 
-        protected override void OnInitialized() {
+        /*protected override void OnInitialized() {
             base.OnInitialized();
 
             Employees = MockData.Employees;
+        }*/
+
+        protected async override Task OnInitializedAsync() {
+            var data = await HttpClient.GetFromJsonAsync<List<Employee>>("https://localhost:7085/api/employee");
+            Employees = data;
         }
 
         public void EmployeePopupRequested(Employee selectedEmployee) {
