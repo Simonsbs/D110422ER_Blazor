00e45c1974e15d16d0b14cc1bf9ca8e3442e758b
Simon B.Stirling <simonsbs@gmail.com>
2023-06-08 20:35:06 +0300

    Finished form basics

--- MyShop_WabAssembly/MyShop_WabAssembly/Pages/EmployeeEdit.razor.cs
+++ MyShop_WabAssembly/MyShop_WabAssembly/Pages/EmployeeEdit.razor.cs
@@ -15,26 +15,56 @@ namespace MyShop.UI.WebAssembly.Pages {
 
         public Employee Employee { get; set; } = new Employee();
 
+        public bool IsSaved { get; set; } = false;
+        public string DisplayMessage { get; set; } = String.Empty;
+        public string AlertType { get; set; } = "alert-primary";
+
+        protected async override Task OnParametersSetAsync() {
+            await OnInitializedAsync();
+        }
+
         protected async override Task OnInitializedAsync() {
             if (ID.HasValue) { 
                 Employee = await EmployeeService.GetEmployeeById(ID.Value);
+            } else {
+                Employee = new Employee();
             }
         }
 
+        private void SetMsg(string msg, bool isSuccess = true) {
+            DisplayMessage = msg;
+            AlertType = isSuccess ? "alert-success" : "alert-warning";
+        }
+
         public async Task HandleValidSubmit() {
+            IsSaved = false;
+            DisplayMessage = string.Empty;
+
             if (ID.HasValue && ID.Value > 0) {
                 // EDIT / UPDATE
 
                 await EmployeeService.UpdateEmployee(Employee);
+
+                IsSaved = true;
+                SetMsg("The employee was updated");
             } else {
                 // NEW / INSERT
 
                 var employee = await EmployeeService.AddEmployee(Employee);
+
+                if (employee == null) {
+                    IsSaved = false;
+                    SetMsg("There was an issue saving the employee", false);
+                } else {
+                    IsSaved = true;
+                    SetMsg("The employee was added");                    
+                }
             }
         }
 
         public void HandleInvalidSubmit() {
-
+            IsSaved = false;
+            SetMsg("There was an issue saving the employee", false);
         }
 
         public async void HandleOnDelete() {
@@ -43,6 +73,10 @@ namespace MyShop.UI.WebAssembly.Pages {
             }
             await EmployeeService.DeleteEmployee(ID.Value);
 
+            NavigateToList();
+        }
+
+        public void NavigateToList() {
             NavigationManager.NavigateTo("/employees");
         }
     }
